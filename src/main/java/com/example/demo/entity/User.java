47fb3ba5edb02f6c518a01d3cb045328aa1a8350
Tobiassssld <158186817+Tobiassssld@users.noreply.github.com>
2025-07-28 16:39:51 +0800

    connect to MySQL database

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -12,8 +12,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true, nullable = false)
     private String username;
 
+    @Column(nullable = false)
     private String password;
 
 }
